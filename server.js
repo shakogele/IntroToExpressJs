ea5d24a602123b9bd8f7d6424bf2b303cee598c4
shakogele <shakogele@dspro.io>
2018-11-16 15:47:16 +0400

    Start Script and Heroku PORT

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
+const port = process.env.PORT || 3000;
+
 var app = express();
 
 // http Routes
@@ -60,6 +62,6 @@ app.get('/bad', (request, response) => {
   response.send(error);
 });
 
-app.listen(3000, () => {
-  console.log("Server is up on port 3000")
+app.listen(port, () => {
+  console.log(`Server is up on port ${port}`)
 });
